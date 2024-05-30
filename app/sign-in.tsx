a92ac1a3f92fe83220862c905f3782371edc12c7
cf913 <chaddudz@hotmail.fr>
2024-05-30 15:05:25 +1000

    cleanup

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -2,14 +2,13 @@ import React, {useState} from 'react'
 import {
   Alert,
   StyleSheet,
-  AppState,
   Button,
   TextInput as Input,
   useColorScheme,
   ActivityIndicator,
 } from 'react-native'
 import {supabase} from '@/lib/supabase'
-import {Redirect, router} from 'expo-router'
+import {Redirect} from 'expo-router'
 import {useSession} from '@/stores/session'
 import {AppleSignInButton} from '@/components/AppleSignInButton'
 import Padder from '@/components/Padder'
@@ -54,14 +53,15 @@ export default function Auth() {
     setLoading(false)
   }
 
-  const inputStyles = [styles.input, {borderColor: Colors[theme].gray, color: Colors[theme].text}]
+  const inputStyles = [
+    styles.input,
+    {borderColor: Colors[theme].gray, color: Colors[theme].text},
+  ]
 
   return (
     <ThemedView style={[styles.container, {paddingTop: insets.top}]}>
       <ThemedView style={[styles.verticallySpaced, styles.mt20]}>
         <Input
-          // label="Email"
-          // leftIcon={{ type: 'font-awesome', name: 'envelope' }}
           onChangeText={text => setEmail(text)}
           value={email}
           placeholder="email@address.com"
@@ -71,8 +71,6 @@ export default function Auth() {
       </ThemedView>
       <ThemedView style={styles.verticallySpaced}>
         <Input
-          // label="Password"
-          // leftIcon={{ type: 'font-awesome', name: 'lock' }}
           onChangeText={text => setPassword(text)}
           value={password}
           secureTextEntry={true}
@@ -82,17 +80,30 @@ export default function Auth() {
         />
       </ThemedView>
       <ThemedView style={[styles.verticallySpaced, styles.mt20]}>
-        <Button title="Sign in" disabled={loading} onPress={() => signInWithEmail()} />
+        <Button
+          title="Sign in"
+          disabled={loading}
+          onPress={() => signInWithEmail()}
+        />
       </ThemedView>
       <ThemedView style={styles.verticallySpaced}>
-        <Button title="Sign up" disabled={loading} onPress={() => signUpWithEmail()} />
+        <Button
+          title="Sign up"
+          disabled={loading}
+          onPress={() => signUpWithEmail()}
+        />
       </ThemedView>
       <Padder />
       <Divider />
       <Padder />
       {loading ? (
         <ThemedView
-          style={{justifyContent: 'center', alignItems: 'center', height: 48, width: '100%'}}
+          style={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: 48,
+            width: '100%',
+          }}
         >
           <ActivityIndicator size="large" color={Colors[theme].gray} />
         </ThemedView>
@@ -106,7 +117,6 @@ export default function Auth() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // marginTop: 40,
     padding: 12,
   },
   verticallySpaced: {

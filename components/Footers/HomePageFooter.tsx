e2fbcebd1d2e7ce6e7a28fc79065454fbc6dbc6e
cf913 <chaddudz@hotmail.fr>
2024-07-10 19:35:05 +1000

    testing animation on select default budget modal

--- components/Footers/HomePageFooter.tsx
+++ components/Footers/HomePageFooter.tsx
@@ -5,7 +5,7 @@ import Footer from '../Layout/Footer'
 import { AnimatedView, ThemedView } from '../ThemedView'
 import { useThemeColor } from '@/hooks/useThemeColor'
 import { Fragment, useState } from 'react'
-import { Easing, FadeIn, FadeOut, SlideInDown } from 'react-native-reanimated'
+import { Easing, FadeIn, FadeOut, SlideInDown, useAnimatedStyle, useSharedValue, withDecay } from 'react-native-reanimated'
 import { Pressable, useWindowDimensions } from 'react-native'
 import SelectBudget from '../Selects/SelectBudget'
 import { PADDING, RADIUS, TYPO } from '@/constants/Styles'
@@ -13,8 +13,10 @@ import { BlurView } from 'expo-blur'
 import { ThemedText } from '../ThemedText'
 import Content from '../Layout/Content'
 import Padder from '../Layout/Padder'
+import { Gesture, GestureDetector } from 'react-native-gesture-handler'
 
 export default function HomePageFooter() {
+  const offset = useSharedValue(0)
   const { width, height } = useWindowDimensions()
   // state
   const [showMenu, setShowMenu] = useState(false)
@@ -35,8 +37,29 @@ export default function HomePageFooter() {
     }
   }
 
+  const pan = Gesture.Pan()
+    .onBegin(() => { })
+    .onChange(event => {
+      offset.value += event.changeY
+    })
+    .onEnd(() => { })
+    .onFinalize((event) => {
+      offset.value = withDecay({
+        velocity: event.velocityY,
+        rubberBandEffect: true,
+        clamp: [
+          0,
+          0
+        ],
+      })
+    })
+
   const entering = SlideInDown.duration(200).easing(Easing.out(Easing.ease))
   const exiting = FadeOut.duration(50).easing(Easing.inOut(Easing.quad))
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ translateY: offset.value }]
+  }))
 
   return (
     <Fragment>
@@ -77,61 +100,64 @@ export default function HomePageFooter() {
         </Pressable>
       ) : null}
       <Footer>
-        <ThemedView
-          style={{
-            backgroundColor: 'transparent',
-          }}
-        >
-          {/* TOGGLEABLE MENU TOP */}
-          {showMenu ? (
-            <AnimatedView
-              entering={entering}
+        <GestureDetector gesture={pan}>
+          <ThemedView
+            style={{
+              backgroundColor: 'transparent',
+            }}
+          >
+            {/* TOGGLEABLE MENU TOP */}
+            {showMenu ? (
+              <AnimatedView
+                entering={entering}
+                style={{
+                  paddingVertical: PADDING,
+                  width: '100%',
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                  backgroundColor: 'transparent',
+                  ...animatedStyle
+                }}
+              >
+                <ThemedView
+                  style={{
+                    paddingVertical: PADDING,
+                    flex: 1,
+                    borderRadius: RADIUS + 4
+                  }}
+                >
+                  <Content>
+                    <ThemedText style={{ ...TYPO.title_mini, textAlign: 'center' }}>Default Budget</ThemedText>
+                    <Padder h={.5} />
+                  </Content>
+                  <SelectBudget callback={toggleMenu} />
+                </ThemedView>
+              </AnimatedView>
+            ) : null}
+            {/* MENU BOTTOM */}
+            {!showMenu ? <ThemedView
               style={{
-                paddingVertical: PADDING,
-                width: '100%',
                 flexDirection: 'row',
                 justifyContent: 'space-between',
                 backgroundColor: 'transparent',
               }}
             >
-              <ThemedView
-                style={{
-                  paddingVertical: PADDING,
-                  flex: 1,
-                  borderRadius: RADIUS + 4
-                }}
-              >
-                <Content>
-                  <ThemedText style={{ ...TYPO.title_mini, textAlign: 'center' }}>Default Budget</ThemedText>
-                  <Padder h={.5} />
-                </Content>
-                <SelectBudget callback={toggleMenu} />
-              </ThemedView>
-            </AnimatedView>
-          ) : null}
-          {/* MENU BOTTOM */}
-          {!showMenu ? <ThemedView
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              backgroundColor: 'transparent',
-            }}
-          >
-            <ThemedButton
-              round
-              icon={<Feather name="list" size={24} color={textColor} />}
-              onPress={toggleMenu}
-              title="BUDGET"
-              style={{ zIndex: 99 }}
-            ></ThemedButton>
-            <ThemedButton
-              round
-              onPress={() => router.navigate('add-new-entry')}
-              title="ADD NEW ENTRY"
-              style={{ zIndex: 95 }}
-            ></ThemedButton>
-          </ThemedView> : null}
-        </ThemedView>
+              <ThemedButton
+                round
+                icon={<Feather name="list" size={24} color={textColor} />}
+                onPress={toggleMenu}
+                title="BUDGET"
+                style={{ zIndex: 99 }}
+              ></ThemedButton>
+              <ThemedButton
+                round
+                onPress={() => router.navigate('add-new-entry')}
+                title="ADD NEW ENTRY"
+                style={{ zIndex: 95 }}
+              ></ThemedButton>
+            </ThemedView> : null}
+          </ThemedView>
+        </GestureDetector>
       </Footer>
     </Fragment>
   )

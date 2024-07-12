a77480b40dec58096f707c1ea20030d3ecd1f3a4
cf913 <chaddudz@hotmail.fr>
2024-07-12 13:54:56 +1000

    v36

--- components/Footers/HomePageFooter.tsx
+++ components/Footers/HomePageFooter.tsx
@@ -44,7 +44,7 @@ export default function HomePageFooter() {
       // hide menu, then hide blur
       setShowMenu(false)
       setTimeout(() => setShowBlur(false), 51)
-      setTimeout(() => offset.value = 0, 100)
+      setTimeout(() => (offset.value = 0), 100) // TODO: generalize this
     }
   }
 
